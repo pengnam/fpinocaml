4fef3b3bbd4eddcff2aafdc58b6d5af326a988d4
Sean Ng <seanngpengnam@gmail.com>
2021-04-23 00:53:10 +0800

    Add chapter 2

--- chapter2/lib/a.ml
+++ chapter2/lib/a.ml
@@ -3,3 +3,23 @@ let fib n =
         if (i = 1) then pp
         else fib_tr pp (p + pp) (i-1) in
     fib_tr 0 1 n;;
+
+
+let is_sorted l f =
+    let rec is_sorted_helper l f prev = 
+        match l, prev with
+        (h::t), None -> is_sorted_helper t f (Some h)
+        | (h::t), Some v -> (f v h) && is_sorted_helper t f (Some v)
+        | [], _ -> true in
+    is_sorted_helper l f None;;
+
+let curry f =
+    fun a -> (fun b -> f (a,b));;
+
+let uncurry f = 
+    fun tup -> (f fst tup) (snd tup);;
+
+
+let compose f g = 
+    fun x -> f (g x);;
+
--- chapter2/test/a.ml
+++ chapter2/test/a.ml
@@ -1,9 +1,21 @@
 (*
    Tests for Chapter2.
-*)
+   *)
 let test_fib () =
     let check_fib msg input expected = Alcotest.(check bool) msg true (Fpinocaml_chapter2.A.fib input = expected) in
-  check_fib "fib 1" 1 1;
+    check_fib "fib 1" 1 1;
   check_fib "fib 12" 12 144
 
-let tests = [ ("fib", `Quick, test_fib) ]
+let test_sorted () =
+    let check_sorted msg input f expected = Alcotest.(check bool) msg expected (Fpinocaml_chapter2.A.is_sorted input f) in
+    check_sorted "simple sorted" [1;2;3] (<) true;
+  check_sorted "reversed" [1;2;3] (>) false;
+  check_sorted "empty" [] (>) true
+
+
+let test_curry() = Alcotest.(check int ) "simple curry" 3 (Fpinocaml_chapter2.A.curry (fun tup -> (fst tup) + (snd tup)) 1 2 )
+
+
+
+
+let tests = [ ("fib", `Quick, test_fib);("sorted", `Quick, test_sorted) ;("curry", `Quick, test_curry)]
